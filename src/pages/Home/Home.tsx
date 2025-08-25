b753db6590580cd129d3457a5c61800324fdc62e
py-bay <github.com.tribunal325@passmail.net>
2025-08-25 17:07:52 +0200

    tmp: add some button to trigger modal and i18n

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,30 @@
-import { Box, Typography } from '@mui/joy';
+import { Box, Typography, Button } from '@mui/joy';
 import LanguageSelectorComponent from '@components/LanguageSelectorComponent/LanguageSelectorComponent';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
-import { Button } from '@mui/material';
+import { useState } from 'react';
+import ExamDatesModal from '@/components/Modals/ExamDatesModal/ExamDatesModal';
 
 const Home = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [newsOpen, setNewsOpen] = useState(false);
 
   return (
     <Box sx={{ padding: 2, maxWidth: 700, mx: 'auto' }}>
       <Typography>{t('pages.home.title')}</Typography>
+
       <Button onClick={() => navigate('/weather')}>
         {t('pages.home.weatherButton')}
       </Button>
+
+      <Button onClick={() => setNewsOpen(true)}>
+        {t('pages.home.examDatesButton')}
+      </Button>
+
       <LanguageSelectorComponent />
+
+      <ExamDatesModal open={newsOpen} setOpen={setNewsOpen} />
     </Box>
   );
 };
