7ef54f523ffd9c7b84ff36108fd410a7501033b9
py-bay <github.com.tribunal325@passmail.net>
2025-08-29 14:31:41 +0200

    link button to moal open

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import { Box } from '@mui/joy';
 import LanguageSelectorComponent from '@components/LanguageSelectorComponent/LanguageSelectorComponent';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
 import { Button } from '@mui/joy';
 import StudentInfoHeader from '@/components/StudentInfoHeader/StudentInfoHeader';
+import ExamDatesModal from '@/components/Modals/ExamDatesModal/ExamDatesModal';
 
 const Home = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const [viewExamDates, setViewExamDates] = useState(false);
 
   return (
     <Box sx={{ padding: 2, mx: 'auto', ml: 10, mr: 10 }}>
@@ -26,13 +29,14 @@ const Home = () => {
         <Button variant="outlined" onClick={() => navigate('/weather')}>
           {t('pages.home.buttons.pubSubmission')}
         </Button>
-        <Button variant="outlined" onClick={() => navigate('/weather')}>
+        <Button variant="outlined" onClick={() => setViewExamDates(true)}>
           {t('pages.home.buttons.viewExamDates')}
         </Button>
         <Button variant="solid" onClick={() => navigate('/weather')}>
           {t('pages.home.buttons.downloadPerformanceOverview')}
         </Button>
       </Box>
+      <ExamDatesModal open={viewExamDates} setOpen={setViewExamDates} />
       <LanguageSelectorComponent />
     </Box>
   );
