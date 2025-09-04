8858bf32fa983e97dc66093ef904d0b2cf3acab8
Simon <104859696+py-bay@users.noreply.github.com>
2025-09-04 19:01:08 +0200

    [ASE-373] Nachprüfungsanmeldung Modal (#4)
    * feat: add RetakeRegistrationModal and integrate into Home
    
    * i18n: add translations for RetakeRegistrationModal.tsx
    
    * lint: fix
    
    * ci: avoid duplicate workflow runs on push and PR

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,23 @@
 import { useState } from 'react';
-import { Box } from '@mui/joy';
+import { Box, Button } from '@mui/joy';
 import LanguageSelectorComponent from '@components/LanguageSelectorComponent/LanguageSelectorComponent';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router';
-import { Button } from '@mui/joy';
 import StudentInfoHeader from '@/components/StudentInfoHeader/StudentInfoHeader';
 import ExamDatesModal from '@/components/Modals/ExamDatesModal/ExamDatesModal';
+import RetakeRegistrationModal from '@/components/Modals/RetakeRegistrationModal/RetakeRegistrationModal';
 
 const Home = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
+
   const [viewExamDates, setViewExamDates] = useState(false);
+  const [viewRetakeRegistration, setViewRetakeRegistration] = useState(false);
 
   return (
     <Box sx={{ padding: 2, mx: 'auto', ml: 10, mr: 10 }}>
       <StudentInfoHeader />
+
       <Box
         sx={{
           display: 'flex',
@@ -23,20 +26,32 @@ const Home = () => {
           gap: 2,
         }}
       >
-        <Button variant="outlined" onClick={() => navigate('/weather')}>
+        <Button
+          variant="outlined"
+          onClick={() => setViewRetakeRegistration(true)}
+        >
           {t('pages.home.buttons.re-examinationRegistration')}
         </Button>
+
         <Button variant="outlined" onClick={() => navigate('/weather')}>
           {t('pages.home.buttons.pubSubmission')}
         </Button>
+
         <Button variant="outlined" onClick={() => setViewExamDates(true)}>
           {t('pages.home.buttons.viewExamDates')}
         </Button>
+
         <Button variant="solid" onClick={() => navigate('/weather')}>
           {t('pages.home.buttons.downloadPerformanceOverview')}
         </Button>
       </Box>
+
       <ExamDatesModal open={viewExamDates} setOpen={setViewExamDates} />
+      <RetakeRegistrationModal
+        open={viewRetakeRegistration}
+        setOpen={setViewRetakeRegistration}
+      />
+
       <LanguageSelectorComponent />
     </Box>
   );
