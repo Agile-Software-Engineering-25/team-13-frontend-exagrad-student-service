88eca95202025234ed3d0b881f11e837d5ee5c3a
py-bay <github.com.tribunal325@passmail.net>
2025-09-12 21:59:31 +0200

    Add CssBaseline and GlobalStyles to App component for consistent styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {
   createCustomMuiTheme,
 } from '@agile-software/shared-components';
 import { THEME_ID as MATERIAL_THEME_ID, ThemeProvider } from '@mui/material';
-import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy';
+import { CssBaseline, GlobalStyles, CssVarsProvider as JoyCssVarsProvider } from '@mui/joy';
 import './i18n';
 import { Provider } from 'react-redux';
 import store from '@stores/index.ts';
@@ -27,6 +27,21 @@ function App({ basename }: AppProps) {
           modeStorageKey="joy-mode"
           colorSchemeStorageKey="joy-color-scheme"
         >
+        <CssBaseline />
+          <GlobalStyles
+            styles={(theme) => ({
+              html: {
+                backgroundColor: theme.vars.palette.background.body,
+                minHeight: '100%',
+              },
+              body: {
+                backgroundColor: theme.vars.palette.background.body,
+                minHeight: '100vh',
+                margin: 0,
+                padding: 0,
+              },
+            })}
+          />
           <BrowserRouter basename={basename}>
             <RoutingComponent />
           </BrowserRouter>
