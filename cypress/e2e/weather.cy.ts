d5e14b07797b3b848cb44d97f541147c1446c8b5
Leonore <152623475+MCLemuri@users.noreply.github.com>
2025-10-06 11:13:50 +0200

    [ASE-750]-PubModal (#13)
    * add date input fields
    
    * add usePubDocuments to use api
    
    * fix error handling
    
    * add uploaded files sheet
    
    * update file upload handling
    
    * update pub modal
    
    * add i18n translations
    
    * add dropzone reset
    
    * remove redundancies
    
    * add rage error message
    
    * add confimation modal
    
    * lint fixes
    
    * use Dropzone Component
    
    * use FileChip component
    
    * change pub upload button position
    
    * fix linting
    
    * remove demo test
    
    * remove dropzone types
    
    * remove unnecessary FormControl
    
    * add production base url
    
    * add remove mock data comment
    
    * add overlapping dates check
    
    * add uploaded file onClick
    
    * lint fix
    
    * lint fix
    
    ---------
    
    Co-authored-by: py-bay <github.com.tribunal325@passmail.net>

--- cypress/e2e/weather.cy.ts
+++ cypress/e2e/weather.cy.ts
@@ -3,14 +3,5 @@ describe('Weather Page', () => {
     cy.visit('/weather');
     cy.contains('Wetter'); // change text as needed
   });
-  it('should display the current temperature', () => {
-    cy.visit('/weather');
 
-    cy.get('#root > div > div > button').click();
-    cy.wait('@getCurrentWeather');
-
-    cy.get('p.MuiTypography-root:nth-child(5)')
-      .should('exist')
-      .should('contain.text', '20.2 °C'); // change selector as needed
-  });
 });
