ae53af079d353dfe88a1cbd8f76687026a4e7308
MCLemuri <elefine@mail.de>
2025-10-06 09:08:01 +0200

    add production base url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-const BACKEND_BASE_URL = 'http://localhost:8080/api/v1/exagrad/student';
+const BACKEND_BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL || 'http://localhost:8080/api/v1/exagrad/student';
 
 export { BACKEND_BASE_URL };
