52198d98ca5bcd35d4e61e14f04e234008c3ec3c
py-bay <github.com.tribunal325@passmail.net>
2025-10-06 20:40:13 +0200

    add TODO comments for further implementation of LecturerFilesSection

--- src/components/LecturerFilesSection/LecturerFilesSection.tsx
+++ src/components/LecturerFilesSection/LecturerFilesSection.tsx
@@ -43,6 +43,8 @@ const LecturerFilesSection = ({ files }: LecturerFilesSectionProps) => {
           width: '100%',
         }}
       >
+        // TODO: handle empty state
+        // TODO: add onClick to download file when mock data is replaced
         {files.map((file, index) => (
           <FileChip
             key={index}
