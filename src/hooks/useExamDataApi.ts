ea95e9553ad7b9c8cd445612ff7e1a1a0ee8bf77
MCLemuri <elefine@mail.de>
2025-10-30 10:35:43 +0100

    delete getOneExamData

--- src/hooks/useExamDataApi.ts
+++ src/hooks/useExamDataApi.ts
@@ -6,25 +6,13 @@ import type { ExamDataResponse } from '@/@custom-types/examData';
 const useExamDataApi = () => {
   const axiosInstance = useAxiosInstance(BACKEND_BASE_URL);
 
-  // GET Data of One Exam by examId
-
-  const getOneExam = useCallback(
-    async (examId: string): Promise<ExamDataResponse> => {
-      const response = await axiosInstance.get('/data/exam', {
-        params: { examId },
-      });
-      return response.data.data as ExamDataResponse;
-    },
-    [axiosInstance]
-  );
-
   // GET Data of all Exams
   const getAllExams = useCallback(async (): Promise<ExamDataResponse[]> => {
     const response = await axiosInstance.get('/data/exams', {});
     return response.data as ExamDataResponse[];
   }, [axiosInstance]);
 
-  return { getOneExam, getAllExams };
+  return { getAllExams };
 };
 
 export default useExamDataApi;
