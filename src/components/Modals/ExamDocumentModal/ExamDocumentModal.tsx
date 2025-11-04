f86ab5f9b24a57dc565d34db9ebe28ef4a9d5b18
MCLemuri <elefine@mail.de>
2025-11-04 15:56:21 +0100

    fix isDeadlinePassed check

--- src/components/Modals/ExamDocumentModal/ExamDocumentModal.tsx
+++ src/components/Modals/ExamDocumentModal/ExamDocumentModal.tsx
@@ -46,9 +46,17 @@ const ExamDocumentModal = ({
   const loadingState = useTypedSelector((state) => state.examDocuments.state);
 
   const isDeadlinePassed = useCallback(() => {
-    if (!assessment?.date) return false;
-    return new Date() > new Date(assessment.date);
-  }, [assessment]);
+  if (!assessment?.date) return false;
+
+  const match = assessment.date.match(/(\d{2})\.(\d{2})\.(\d{4}),\s*(\d{2}):(\d{2})/);
+  if (!match) return false;
+
+  const [_, day, month, year, hours, minutes] = match.map(Number);
+  const deadline = new Date(year, month - 1, day, hours, minutes, 0, 0);
+
+  return new Date() > deadline;
+}, [assessment?.date]);
+
 
   const canDelete = !isDeadlinePassed();
 
