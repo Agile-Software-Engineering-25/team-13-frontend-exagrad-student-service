0e02700389476d9a0a34ec046cd09206e1b648d3
Juna <annika.schatter@web.de>
2025-11-06 13:18:57 +0100

    trying fixes

--- src/hooks/useStudentDataApi.ts
+++ src/hooks/useStudentDataApi.ts
@@ -2,17 +2,20 @@ import { useCallback } from 'react';
 import useAxiosInstance from '@hooks/useAxiosInstance';
 import { BACKEND_BASE_URL } from '@/config';
 import type { StudentDataResponse } from '@/@custom-types/studentData';
+import useUser from './useUser';
 
 const useStudentDataApi = () => {
   const axiosInstance = useAxiosInstance(BACKEND_BASE_URL);
+  const { getUserId } = useUser();
 
   // GET Data of Student 
-  const getStudent = useCallback(
-    async (studentId: string): Promise<StudentDataResponse[]> => {
-      const response = await axiosInstance.get(`/students/${studentId}`);
-    return response.data.data as StudentDataResponse[];
+  const getStudent = useCallback(async (): Promise<StudentDataResponse[]> => {
+    const studentId = getUserId();
+    console.log("HI" + studentId);
+    const response = await axiosInstance.get(`/students/${studentId}`);
+    return response.data as StudentDataResponse[];
     },
-    [axiosInstance]
+    [axiosInstance, getUserId]
   );
 
   return { getStudent };
